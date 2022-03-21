a05a517b6ea7b2268a33013bf9dfcd747494be66
Christophe Camel <camel.christophe@gmail.com>
2022-03-21 15:26:40 +0100

    build(deps): add kotest dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,14 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    val kotestVersion = "5.2.1"
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
 }
 
-tasks.test {
+tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
 
