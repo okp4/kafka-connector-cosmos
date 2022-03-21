b3b90a48b5fb6b2fb00ac2903c2ef8ca4b797309
Christophe Camel <camel.christophe@gmail.com>
2022-03-21 17:42:08 +0100

    feat: implement faked source connector

---
+++ src/main/kotlin/com/okp4/connect/cosmos/CosmosSourceTask.kt
@@ -0,0 +1,64 @@
+package com.okp4.connect.cosmos
+
+import org.apache.kafka.connect.data.Schema
+import org.apache.kafka.connect.source.SourceRecord
+import org.apache.kafka.connect.source.SourceTask
+
+class CosmosSourceTask : SourceTask() {
+    private var chainId: String? = null
+    private var node: String? = null
+    private var topic: String? = null
+
+    private var sourcePartition: Map<String, String?> = mapOf()
+    private var height: Long = 0
+
+    override fun version(): String = CosmosSourceConnector.VERSION
+
+    override fun start(props: Map<String, String>) {
+        chainId = props[CosmosSourceConnector.CHAIN_ID_CONFIG]
+        node = props[CosmosSourceConnector.NODE_CONFIG]
+        topic = props[CosmosSourceConnector.TOPIC_CONFIG]
+
+        sourcePartition = mapOf(
+            CHAIN_ID_FIELD to chainId,
+            NODE_FIELD to node
+        )
+    }
+
+    @Throws(InterruptedException::class)
+    override fun poll(): List<SourceRecord>? =
+        // TODO: ⚠️ fake code - implement-me correctly here!
+        when {
+            (0..10).shuffled().last() == 5 -> {
+                height++
+
+                listOf(
+                    SourceRecord(
+                        sourcePartition,
+                        mapOf(HEIGHT_FIELD to height),
+                        topic,
+                        null,
+                        null,
+                        null,
+                        Schema.STRING_SCHEMA,
+                        "This is payload for block $height",
+                        System.currentTimeMillis()
+                    )
+                )
+            }
+            else -> {
+                Thread.sleep(1000)
+                null
+            }
+        }
+
+    override fun stop() {
+        // TODO implement-me!
+    }
+
+    companion object {
+        const val CHAIN_ID_FIELD = "chain-id"
+        const val NODE_FIELD = "node"
+        const val HEIGHT_FIELD = "height"
+    }
+}
