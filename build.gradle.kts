b687fcbe6a8834aba4b648fd94da281999e0ace3
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2022-03-22 18:44:56 +0100

    build: produce a fatjar

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,23 @@ dependencies {
     testImplementation("io.kotest:kotest-property:$kotestVersion")
 }
 
+tasks {
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
+        archiveClassifier.set("standalone")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to application.mainClass)) }
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } +
+            sourcesMain.output
+        from(contents)
+    }
+    build {
+        dependsOn(fatJar)
+    }
+}
+
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
